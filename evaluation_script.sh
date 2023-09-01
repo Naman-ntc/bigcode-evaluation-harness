a5696fc28d95ee86b4eadea1fe71e3a7cdf91bba
Naman Jain <naman1205jain@gmail.com>
2023-08-31 17:09:05 -0700

    fix script

--- evaluation_script.sh
+++ evaluation_script.sh
@@ -2,4 +2,4 @@ PREFIX=$1
 TEMP=$2
 FILE=$3
 
-python main.py --use_auth_token --trust_remote_code --tasks apps-introductory-cfstyle --batch_size 30 --n_samples 30     --max_length_generation 2048 --precision bf16 --allow_code_execution --temperature $TEMP --load_generations_path $FILE ----metric_output_path "evaluation_results_${PREFIX}_${TEMP}.json"
+python main.py --use_auth_token --trust_remote_code --tasks apps-introductory-cfstyle --batch_size 30 --n_samples 30     --max_length_generation 2048 --precision bf16 --allow_code_execution --temperature $TEMP --load_generations_path $FILE --metric_output_path "evaluation_results_${PREFIX}_${TEMP}.json"
